c1d8b08ab09c5af120993a55d1236c8c15d51600
JJampion <122892107+nightodaygamestudio@users.noreply.github.com>
2026-02-01 16:11:30 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'mia-stock-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://unpkg.com/vue@3/dist/vue.global.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Installation: Alles herunterladen und speichern
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivierung: Alte Caches löschen (falls Updates kommen)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Abruf: Immer erst im Cache schauen, dann im Netz
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
